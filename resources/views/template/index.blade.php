cfcfb4f990b7bb7a523d97b0a9efcfb606cbd91f
johndavis1999 <jdyglol123@gmail.com>
2024-11-17 03:49:18 -0500

    customize theme

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-bs-theme="light">
+<html lang="en">
     <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Template | Admin</title>
@@ -16,9 +16,77 @@
         <link href="./admin/css/bootstrap-extended.css" rel="stylesheet">
         <link href="./admin/css/main.css" rel="stylesheet">
         <link href="./admin/css/responsive.css" rel="stylesheet">
+        <link href="./admin/css/dark-theme.css" rel="stylesheet">
+        <link href="./admin/css/blue-theme.css" rel="stylesheet">
+        <link href="./admin/css/semi-dark.css" rel="stylesheet">
+        <link href="./admin/css/bordered-theme.css" rel="stylesheet">
     </head>
     <body class="">
         @include('template.main')
+        <!--start switcher-->
+        <button 
+            class="btn btn-grd btn-grd-primary position-fixed bottom-0 end-0 m-3 d-flex align-items-center gap-2" 
+            type="button" 
+            data-bs-toggle="offcanvas" 
+            data-bs-target="#staticBackdrop"
+        >
+          <i class="material-icons-outlined">tune</i>Customize
+        </button>
+        
+        <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="staticBackdrop">
+          <div class="offcanvas-header border-bottom h-70">
+            <div class="">
+              <h5 class="mb-0">Theme Customizer</h5>
+              <p class="mb-0">Customize your theme</p>
+            </div>
+            <a href="javascript:;" class="primaery-menu-close" data-bs-dismiss="offcanvas">
+              <i class="material-icons-outlined">close</i>
+            </a>
+          </div>
+          <div class="offcanvas-body">
+            <div>
+              <p>Theme variation</p>
+              <div class="row g-3">
+                <div class="col-12 col-xl-6">
+                  <input type="radio" class="btn-check" name="theme-options" id="BlueTheme" checked="">
+                  <label class="btn btn-outline-secondary d-flex flex-column gap-1 align-items-center justify-content-center p-4" for="BlueTheme">
+                    <span class="material-icons-outlined">contactless</span>
+                    <span>Blue</span>
+                  </label>
+                </div>
+                <div class="col-12 col-xl-6">
+                  <input type="radio" class="btn-check" name="theme-options" id="LightTheme">
+                  <label class="btn btn-outline-secondary d-flex flex-column gap-1 align-items-center justify-content-center p-4" for="LightTheme">
+                    <span class="material-icons-outlined">light_mode</span>
+                    <span>Light</span>
+                  </label>
+                </div>
+                <div class="col-12 col-xl-6">
+                  <input type="radio" class="btn-check" name="theme-options" id="DarkTheme">
+                  <label class="btn btn-outline-secondary d-flex flex-column gap-1 align-items-center justify-content-center p-4" for="DarkTheme">
+                    <span class="material-icons-outlined">dark_mode</span>
+                    <span>Dark</span>
+                  </label>
+                </div>
+                <div class="col-12 col-xl-6">
+                  <input type="radio" class="btn-check" name="theme-options" id="SemiDarkTheme">
+                  <label class="btn btn-outline-secondary d-flex flex-column gap-1 align-items-center justify-content-center p-4" for="SemiDarkTheme">
+                    <span class="material-icons-outlined">contrast</span>
+                    <span>Semi Dark</span>
+                  </label>
+                </div>
+                <div class="col-12 col-xl-6">
+                  <input type="radio" class="btn-check" name="theme-options" id="BoderedTheme">
+                  <label class="btn btn-outline-secondary d-flex flex-column gap-1 align-items-center justify-content-center p-4" for="BoderedTheme">
+                    <span class="material-icons-outlined">border_style</span>
+                    <span>Bordered</span>
+                  </label>
+                </div>
+              </div><!--end row-->
+            </div>
+          </div>
+        </div>
+        <!--start switcher-->
         <script src="./admin/js/bootstrap.bundle.min.js"></script>
         <script src="./admin/js/jquery.min.js"></script>
         <script src="./admin/plugins/metismenu/metisMenu.min.js"></script>
